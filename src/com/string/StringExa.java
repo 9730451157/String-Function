ff9ef81a18f3c599667d32e7baa807c44b5c5221
unknown <pankajtayade369@gmail.com>
2022-01-19 19:26:58 +0530

    String Function

---
+++ src/com/string/StringExa.java
@@ -0,0 +1,16 @@
+package com.string;
+
+public class StringExa {
+
+	public static void main(String[] args) {
+		
+		String str="Infosys is my company i have job in infosys";
+		
+		System.out.println(str.length());
+		
+		System.out.println(str.charAt(5));
+		System.out.println(str.indexOf('i'));
+
+	}
+
+}
